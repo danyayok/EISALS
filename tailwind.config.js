a4b1b13c6e81edf472e1d6a5557f74872873c5d8
Danial <danildanfuntastic@gmail.com>
2026-02-14 19:00:27 +0300

    Невероятный фронтенд

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/templates/**/*.html",
+    "./app/**/*.py"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
